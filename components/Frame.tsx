29a4fa9dcc3c2ccd1154f78a10264c2011a76cf1
Ethan Flute <ethansamf@gmail.com>
2022-12-31 13:52:27 -0600

    Add ScrollingGrid component.

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -1,33 +1,24 @@
-import { stringLiteral } from '@babel/types';
-import React, { ReactElement } from 'react';
-import { ColorValue, View, ViewStyle } from 'react-native';
+import React from 'react';
+import { ColorValue, View } from 'react-native';
 
 interface FrameProps {
-    size: number,
     radius?: number,
-    padding?: number,
     backgroundColor?: ColorValue,
     gradient?: string, // for help with the gradients see: https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/linear-gradient
     image?: string,
     shadow?: number,
     children?: React.ReactNode,
-    style?: {}
+    style?: any
 };
 
-const Frame: React.FC<FrameProps> = ({ size, radius, padding, backgroundColor, gradient, image, shadow, children, style }) => {
+const Frame: React.FC<FrameProps> = ({ radius, backgroundColor, gradient, image, shadow, children, style }) => {
     return (
         <View style={{
             ...style,
-            width: size || 100,
-            height: size || 100,
             backgroundColor: backgroundColor || 'rgb(255, 0, 0)',
             //@ts-ignore
             backgroundImage: image || gradient || null,
             borderRadius: radius || 8,
-            paddingTop: padding || 5,
-            paddingRight: padding || 5,
-            paddingBottom: padding || 5,
-            paddingLeft: padding || 5,
             boxShadow: '0 2px 4px rgba(0, 0, 0,' + shadow || 0 + ')',
         }}>
             {children}
