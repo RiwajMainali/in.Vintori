01a607492e85c0d1f1ecb6e118947e80472e6e6b
kasiyo <kaiasiripanyo@gmail.com>
2023-01-10 16:18:02 -0600

    added routing
    router set up via react-router-dom package, added pages folder with optional root page file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Product({ match }: { match: { params: { id: string } } }) {
+  return <h2>This is a page for product with ID: {match.params.id} </h2>;
+}
+
+/*function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products/1">First Product</Link>
+            </li>
+            <li>
+              <Link to="/products/2">Second Product</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/products/:id" component={Product} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;*/
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+//root should probs include navbar
+const Root = () => /*<div>Root heading</div>*/ <App />;
+const Dashboard = () => <div>Dashboard</div>
+const router = createBrowserRouter(createRoutesFromElements(
+        <Route path="/" element={<Root />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+    
+));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/*<App />*/}
   </React.StrictMode>
 );
 
